d8a96ac05c0f2d41cbe73912f2b74470c3ba0317
instant rice <canetaei@gmail.com>
2024-04-16 18:46:20 +0900

    0416

---
+++ chap03-method-and-api/src/main/java/com/ohgiraffers/section03/math/Application01.java
@@ -0,0 +1,23 @@
+package main.java.com.ohgiraffers.section03.math;
+
+public class Application01 {
+    public static void main(String[] args) {
+
+        /*
+        * API란?
+        * application programing interface는 응용프로그램에서 사용할 수 있도록
+        * 운영체제나 프로그래밍 언어가 제공하는 기능을 제어할 수 있도록 만든 인터페이스를 뜻한다.
+        * 쉽게 말해서 우리가 구현할 수 없거나 구현하기 번거로운 기능들을 jdk를 설치하면 사용할 수 있도록 제공해놓은 소스코드(클래스나 인터페이스)들을 의미한다.
+        * 더 쉽게 누군가 작성해놓은 소스코드를 가져다 사용하는 것이다.
+        * */
+
+        //펌웨어 임베디드 웹 프론트 백 인공지능 정말 만타
+        //잘 만들어놓은걸 가져다가 커스터마이징 해서 잘만 써도 좋은 개발자~~~~
+        //바로 내가 잘 하는 것. 무에서 유는 없다.
+
+        System.out.println(java.lang.Math.abs(4.5));}
+          // java라는 클래스
+
+    }
+
+
